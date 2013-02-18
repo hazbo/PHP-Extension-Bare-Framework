b657bd0d933902432846be090a1a16fc0bbc4f46
vagrant <vagrant@precise64.(none)>
2013-02-18 20:07:37 +0000

    Removed reference from old code

--- php_framework.h
+++ php_framework.h
@@ -1,11 +1,11 @@
-#ifndef PHP_HELLO_H
+#ifndef PHP_FRAMEWORK_H
 #define PHP_FRAMEWORK 1
 #define PHP_FRAMEWORK_VERSION "1.0"
 #define PHP_FRAMEWORK_EXTNAME "framework"
 
 PHP_FUNCTION(test);
 
-extern zend_module_entry hello_module_entry;
-#define phpext_hello_ptr &hello_module_entry
+extern zend_module_entry framework_module_entry;
+#define phpext_hello_ptr &framework_module_entry
 
 #endif
